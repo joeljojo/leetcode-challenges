69821474b53330825487d5223d4c987295a04a10
joeljojo <joelpamphyl@gmail.com>
2023-09-08 20:39:35 +0300

    Solved Goal Parser Interpretation Challenge

---
+++ leetcode/easy/goalParserIntepreter.js
@@ -0,0 +1,48 @@
+/**Problem: Goal Parser Interpretation
+
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"*/
+
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function (command) {
+  let ans = "";
+  let splitCommand = command.split("");
+  let n = splitCommand.length;
+  for (let i = 0; i < n; i++) {
+    if (splitCommand[i] === "G") {
+      ans += "G";
+    } else if (splitCommand[i] === "(" && splitCommand[i + 1] === ")") {
+      ans += "o";
+    } else if (
+      splitCommand[i] === "(" &&
+      splitCommand[i + 1] === "a" &&
+      splitCommand[i + 2] === "l" &&
+      splitCommand[i + 3] === ")"
+    ) {
+      ans += "al";
+    }
+  }
+  return ans;
+};
