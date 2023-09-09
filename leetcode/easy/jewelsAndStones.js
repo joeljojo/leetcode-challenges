228dd5736c2484ee56b6d61116ef120540108768
joeljojo <joelpamphyl@gmail.com>
2023-09-09 16:34:43 +0300

    Solved Jewels and Stones Challenge

---
+++ leetcode/easy/jewelsAndStones.js
@@ -0,0 +1,29 @@
+/**Problem: Jewels and Stones
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0 */
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+  let count = 0;
+  for (let i = 0; i < jewels.length; i++) {
+    for (let j = 0; j < stones.length; j++) {
+      if (jewels.at(i) === stones.at(j)) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
