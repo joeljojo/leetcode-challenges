5dcd6de95909424d4e9efe732977bc95653300d1
joeljojo <joelpamphyl@gmail.com>
2023-09-14 15:26:30 +0300

    Solved 3Sum Challenge

---
+++ leetcode/medium/3Sum.js
@@ -0,0 +1,55 @@
+/**Problem: 3Sum
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0. */
+
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let n = nums.length;
+  let triplets = new Set();
+
+  for (let i = 0; i < n; i++) {
+    let left = i + 1;
+    let right = n - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        triplets.add([nums[i], nums[left], nums[right]].join(",")); // Store as a comma-separated string to ensure uniqueness.
+        left++;
+        right--;
+        `         `;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return Array.from(triplets).map((triplet) => triplet.split(",").map(Number));
+};
